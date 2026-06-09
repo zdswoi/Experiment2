a952559f5e10513584265a98304c2a2c37c27ddf
zdswoi <94736722+zdswoi@users.noreply.github.com>
2026-06-09 09:47:25 +0800

    Update train.py

--- YOLOv8/train.py
+++ YOLOv8/train.py
@@ -3,5 +3,5 @@ from ultralytics import YOLO
 
 model = YOLO("./yolov8n.pt") 
 
-model.train(data="data-1.yaml",workers=0,epochs=10,batch=16,imgsz=640) 
-# yolov5===data="data-1.yaml",workers=4,epochs=100,batch=2//////workers如果是wins系统设置成1就可以
+model.train(data="data.yaml",workers=0,epochs=10,batch=16,imgsz=640) 
+# yolov5===data="data.yaml",workers=4,epochs=100,batch=2//////workers如果是wins系统设置成1就可以
